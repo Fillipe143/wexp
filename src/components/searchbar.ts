2c60ce4ed662135db8c56132a7321ccb168f8528
Fillipe143 <fillipefbatist@gmail.com>
2026-05-09 16:03:47 -0300

    Adicionado layout do inicio

--- src/components/searchbar.ts
+++
@@ -1,9 +0,0 @@
-import $ from "jquery";
-
-$(".input-container.search").toArray().forEach(searchbar => {
-    const inputField = $(searchbar).find("input");
-
-    $(searchbar).find(".close-icon").on("click", _ => {
-        inputField.val("");
-    });
-});
--- src/lib/markdown_viewer/index.ts
+++
@@ -1,6 +0,0 @@
-import { tokenizer } from "./lexer/tokenizer";
-import { parseTokens } from "./lexer/parser";
-
-export function mdToHtml(source: string): string {
-    return parseTokens(tokenizer(source));
-}
