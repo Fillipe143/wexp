c042f73e03d0271165d87a41af2e59655451b7d6
Pedro L <pedrolcs30@gmail.com>
2026-08-30 17:26:20 -0300

    Fix API URL again for prod

--- src/main/resources/static/pages/game/game.js
+++ src/main/resources/static/pages/game/game.js
@@ -26,7 +26,7 @@ window.onload = () => {
     content.innerHTML = "Carregando conquistas...";
 
     try {
-      const response = await fetch(`api/games/${gameId}/achievements`);
+      const response = await fetch(`/api/games/${gameId}/achievements`);
 
       const data = await response.json();
 
